b401a8f31c94c3e9dd65e8d179d0b279a92cf550
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-11-04 18:33:31 +0530

    Update insertion_in_circular_singly_linked_list.c

--- insertion_in_circular_singly_linked_list.c
+++ insertion_in_circular_singly_linked_list.c
@@ -1,8 +1,77 @@
 #include<stdio.h>
-void beg_insert(int); 
-void 
+void beg_insert(); 
+void lastinsert();  
 struct node  
 {  
     int data;  
     struct node *next;  
 };  
+struct node* head;
+int main(){
+}
+void beginsert()  
+{  
+    struct node *ptr,*temp;   
+    int item;   
+    ptr = (struct node *)malloc(sizeof(struct node));  
+    if(ptr == NULL)  
+    {  
+        printf("\nOVERFLOW");  
+    }  
+    else   
+    {  
+        printf("\nEnter the node data?");  
+        scanf("%d",&item);  
+        ptr -> data = item;  
+        if(head == NULL)  
+        {  
+            head = ptr;  
+            ptr -> next = head;  
+        }  
+        else   
+        {     
+            temp = head;  
+            while(temp->next != head)  
+                temp = temp->next;  
+            ptr->next = head;   
+            temp -> next = ptr;   
+            head = ptr;  
+        }   
+        printf("\nnode inserted\n");  
+    }  
+              
+}  
+void lastinsert()  
+{  
+    struct node *ptr,*temp;   
+    int item;  
+    ptr = (struct node *)malloc(sizeof(struct node));  
+    if(ptr == NULL)  
+    {  
+        printf("\nOVERFLOW\n");  
+    }  
+    else  
+    {  
+        printf("\nEnter Data?");  
+        scanf("%d",&item);  
+        ptr->data = item;  
+        if(head == NULL)  
+        {  
+            head = ptr;  
+            ptr -> next = head;    
+        }  
+        else  
+        {  
+            temp = head;  
+            while(temp -> next != head)  
+            {  
+                temp = temp -> next;  
+            }  
+            temp -> next = ptr;   
+            ptr -> next = head;  
+        }  
+          
+        printf("\nnode inserted\n");  
+    }  
+  
+}  
