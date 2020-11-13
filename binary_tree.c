b030f660a319cd4d1281c93413b93e9c68a7e8db
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-11-13 17:24:50 +0530

    Update binary_tree.c

--- binary_tree.c
+++ binary_tree.c
@@ -1,12 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node{
-  struct node *lnode;
-  int val;
-  struct node *rnode;
+struct Node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+
+struct node* create(int data) {
+    //return the pointer for the new node
+    struct node *ptr= (struct node *)malloc(sizeof(struct node));
+    ptr->data= data;
+    ptr->left = NULL;
+    ptr->right = NULL;
+    return ptr;
 }
 
+void preorder_traversal(struct node *root) {
+    if (root != NULL) {
+        printf("%d\n", root->data);
+        preorder_traversal(root->left);
+        preorder_traversal(root->right);
+    }
+    else return;
+}
 
-int main(){
-  return 0;
+void inorder_traversal(struct node *root) {
+    if(root != NULL){
+        inorder_traversal(root->left);
+        printf("%d\n", root->data);
+        inorder_traversal(root->right);
+    }
+    else return;
+}
+
+void postorder_traversal(struct node *root) {
+    if(root != NULL){
+        postorder_traversal(root->left);
+        postorder_traversal(root->right);
+        printf("%d\n",root->data);
+    }
+    else return;
+}
+
+int main() {
+  
 }
