318052be9db0a66ec10d9fe3d8e9ceeb86abc462
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-11-17 16:46:00 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -7,3 +7,28 @@
 +------------+-----------+--------------+------------+
   
 */
+    #include<stdio.h>   
+    int main ()  {  
+        int item,n, i,flag=0; 
+        printf("Enter number of Elements : \n");
+        scanf("%d",&n);
+        int a[n];
+        printf("Enter the array : \n");
+        for(i=0;i<n;i++)
+            scanf("%d",&a[i]);
+        
+        printf("Enter Element to search for : \n");  
+        scanf("%d",&item);  
+        for (i = 0; i< n; i++){  
+            if(a[i] == item){  
+                flag = i+1;  
+                break;  
+            }  
+        }   
+        if(flag != 0){  
+            printf("\nItem found at location %d\n",flag);  
+        }  
+        else{  
+            printf("\nItem not found\n");   
+        }  
+    }   
