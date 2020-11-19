01b3ac00d233da5a65cd92c399eba14d566ac98a
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-11-19 12:49:22 +0530

    Update doubly_linked_list_implementation.c

--- doubly_linked_list_implementation.c
+++ doubly_linked_list_implementation.c
@@ -1,9 +1,7 @@
+/*Doubly linked list implementation in c*/
 /*
-Doubly linked list implementation in c
-Doubly linked list is a complex type of linked list in which a node contains a pointer to the previous 
-as well as the next node in the sequence.
-Therefore, in a doubly linked list, a node consists of three parts: node data, pointer to the next node in sequence (next pointer) ,
-pointer to the previous node (previous pointer). 
+/Doubly linked list is a complex type of linked list in which a node contains a pointer to the previous 
+/as well as the next node in the sequence.
 */
 #include<stdio.h>
 #include<stdlib.h>
