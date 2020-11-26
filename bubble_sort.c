1f1bf429426d38f99b3db3ed8588d067a4227034
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-11-26 12:42:28 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -11,4 +11,23 @@
 */
 #include<stdio.h>
 int main(){
+    int i, j,temp;   
+    int a[] = { 98,2,6,1,64,38,73,82,1002,85,43,87};   
+    int n=sizeof(a)/sizeof(a[0]);
+    for(i = 0; i<n; i++)  
+    {  
+        for(j = i+1; j<n; j++)  
+        {  
+            if(a[j] > a[i])  
+            {  
+                temp = a[i];  
+                a[i] = a[j];  
+                a[j] = temp;   
+            }   
+        }   
+    }   
+    printf("Sorted Element List \n");  
+    for(i = 0; i<n; i++){  
+        printf("%d\n",a[i]);  
+    } 
 }
