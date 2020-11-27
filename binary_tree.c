6d2fd083e614ea45e36780ab535bad3cd2e53e1f
Rohan j <rohan.cce@sece.ac.in>
2020-11-27 15:22:44 +0530

    Update binary_tree.c

--- binary_tree.c
+++ binary_tree.c
@@ -46,11 +46,6 @@ int main() {
     struct Node *root= create(4);
     root->left= create(3);
     root->right= create(5);
-    root->left->left= create(1);
-    root->left->right= create(2);
-    root->right->left= create(6);
-    root->right->right= create(7);
-
     printf("printing pre order traversal : \n");
     preorder_traversal(root);
 
