ab3c323a8559ea50fa7a731031b4167c502db40b
Rohan j <rohan.cce@sece.ac.in>
2020-12-05 21:52:38 +0530

    Update linear_search.c

--- searching/linear_search.c
+++ searching/linear_search.c
@@ -1,7 +1,7 @@
 /*
                                                   Linear search 
 
-                                    a linear search or sequential search is a method for finding an element within a list.
+                                    A linear search or sequential search is a method for finding an element within a list.
                                     It sequentially checks each element of the list until a match is found or the whole list
                                     has been searched. A linear search runs in at worst linear time and makes at most n comparisons,
                                     where n is the length of the list
