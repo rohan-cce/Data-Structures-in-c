af5c6574359ab043e3a41e183f663ba9ebec92f2
Rohan j <rohan.cce@sece.ac.in>
2020-12-05 21:45:05 +0530

    Update queue_implementation_using_array.c

--- queues/queue_implementation_using_array.c
+++ queues/queue_implementation_using_array.c
@@ -1,5 +1,9 @@
 /*
         Queue implementation using array
+        A Queue is a linear structure which follows a particular order in which the 
+        operations are performed. The order is First In First Out (FIFO).
+         A good example of a queue is any queue of consumers for a resource where the consumer that
+          came first is served first. 
 */
 #include<stdio.h>
 #include<stdlib.h>
