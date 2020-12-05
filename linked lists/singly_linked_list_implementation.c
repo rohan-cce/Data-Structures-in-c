6f5dc314f70f13a5cb8864c36e576b24e5eab3ff
Rohan j <rohan.cce@sece.ac.in>
2020-12-05 21:24:01 +0530

    Update singly_linked_list_implementation.c

--- linked lists/singly_linked_list_implementation.c
+++ linked lists/singly_linked_list_implementation.c
@@ -1,4 +1,5 @@
-/*Single linked list implementation using dynamic memory allocation
+/*
+Single linked list implementation using dynamic memory allocation
 
     Linked List can be defined as collection of objects called nodes that are randomly stored in the memory.
     A node contains two fields i.e. data stored at that particular address and the pointer which contains the address of the next node in the memory.
