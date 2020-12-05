28cf7b2ffdd2d7727bda8a2d68b21da142f268f7
Rohan j <rohan.cce@sece.ac.in>
2020-12-05 21:20:49 +0530

    Update doubly_linked_list_implementation.c

--- linked lists/doubly_linked_list_implementation.c
+++ linked lists/doubly_linked_list_implementation.c
@@ -4,7 +4,6 @@ Doubly linked list implementation in c
 Doubly linked list is a complex type of linked list in which a node contains a pointer to the previous 
 as well as the next node in the sequence.
 
-/*
                                                             Doubly Linked list
                 +----------------------------------------------------------------------------------+--------------------+
                 |                                   Time complexity                                |   Space Complexity |
