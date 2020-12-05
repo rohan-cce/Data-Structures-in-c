8041758f0ad2ca7385bf1294bbcc7a84dc3d2439
Rohan j <rohan.cce@sece.ac.in>
2020-12-05 21:55:42 +0530

    Update bubble_sort.c

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -3,7 +3,8 @@
 
                                 Bubble sort is a simple sorting algorithm. 
                                 This sorting algorithm is comparison-based algorithm in which each pair of adjacent elements is
-                                compared and the elements are swapped if they are not in order. This algorithm is not suitable for large data sets as its average and worst case complexity are of Ο(n2) where n is the number of items.
+                                compared and the elements are swapped if they are not in order.
+                                 This algorithm is not suitable for large data sets as its average and worst case complexity are of Ο(n2) where n is the number of items.
                             +------------+-----------+--------------+------------+
                             | Complexity | Best Case | Average Case | Worst Case |
                             +------------+-----------+--------------+------------+
