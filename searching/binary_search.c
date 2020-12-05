4572a85f253b80114406c5fd84751b706f2b2f73
Rohan j <rohan.cce@sece.ac.in>
2020-12-05 21:49:54 +0530

    Update binary_search.c

--- searching/binary_search.c
+++ searching/binary_search.c
@@ -3,7 +3,7 @@
 
                     
                     Binary Search is a searching algorithm for finding an element's position in a sorted array.
-    Binary search can be implemented only on a sorted list of items. If the elements are not sorted already, we need to sort them first                    
+                    Binary search can be implemented only on a sorted list of items. If the elements are not sorted already, we need to sort them first                    
                                           
                                   +------------+-----------+--------------+------------+
                                   | Complexity | Best Case | Average Case | Worst Case |
