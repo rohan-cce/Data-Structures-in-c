e8b9ae23fe4e68c1cd0f303099db37439fd8300c
Rohan j <rohan.cce@sece.ac.in>
2020-12-05 21:40:24 +0530

    Update circular_queue_implementation.c

--- queues/circular_queue_implementation.c
+++ queues/circular_queue_implementation.c
@@ -1,6 +1,9 @@
 /*
                     Circular Queue Implementation in C using Arrays
 
+                Circular Queue is a linear data structure in which the operations are performed based on FIFO 
+                (First In First Out) principle and the last position is connected back to the first position 
+                to make a circle. It is also called ‘Ring Buffer’. 
 #include<stdio.h>         
 #include<stdlib.h>        
 #define MAX 10       
