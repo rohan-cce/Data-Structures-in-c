addc8f672e8f1a1c2bf5b8adfef533e1a405390d
Rohan j <rohan.cce@sece.ac.in>
2020-12-08 17:12:45 +0530

    update

--- example programs/recursion_check_sorted.c
+++ example programs/recursion_check_sorted.c
@@ -4,11 +4,15 @@
 //Space Complexity O(n) for recursive stack space
 #include<stdio.h>
 int is_array_in_sorted_order(int a[],int n){
+
     if(n==1)    return 1;
+    
     return (a[n-1]<a[n-2])?0:is_array_in_sorted_order(a,n-1);
 }
 int main(){
     int arr[]={1,2,3,4,5};
+
     int size=sizeof(arr)/sizeof(arr[0]);
+
     printf("%d",is_array_in_sorted_order(arr,size));
 }
