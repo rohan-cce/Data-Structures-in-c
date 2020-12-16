6a923f1a4b3c2695c1b3fb8e9b3708f73b82e86e
Rohan j <rohan.cce@sece.ac.in>
2020-12-16 21:55:37 +0530

    Update recursion_check_sorted.c

--- example programs/recursion_check_sorted.c
+++ example programs/recursion_check_sorted.c
@@ -1,1 +1,11 @@
 //Program to check whether the array is sorted or not using recursion
+#include<stdio.h>
+int is_array_in_sorted_order(int a[],int n){
+    if(n==1)    return 1;
+    return (a[n-1]<a[n-2])?0:is_array_in_sorted_order(a,n-1);
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    printf("%d",is_array_in_sorted_order(arr,size));
+}
